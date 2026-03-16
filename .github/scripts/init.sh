43ea1e3ffac54e2d172e3fae39f243f54eee874d
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-16 16:19:59 -0700

    Update init.sh

--- .github/scripts/init.sh
+++ .github/scripts/init.sh
@@ -2,3 +2,7 @@
 chmod +x /workspaces/Hybrid/.github/scripts/install-git.sh
 /workspaces/Hybrid/.github/scripts/install-git.sh
 chmod -x /workspaces/Hybrid/.github/scripts/install-git.sh
+
+chmod +x /workspaces/Hybrid/.github/scripts/configure-git.sh
+/workspaces/Hybrid/.github/scripts/configure-git.sh
+chmod -x /workspaces/Hybrid/.github/scripts/configure-git.sh
