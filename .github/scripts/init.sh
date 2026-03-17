45b900ed562027e5640e7e0c5336b591ee9e04f7
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-16 22:32:52 -0700

    Delete .github/scripts/init.sh

--- .github/scripts/init.sh
+++
@@ -1,8 +0,0 @@
-
-chmod +x .github/scripts/install-git.sh
-.github/scripts/install-git.sh
-chmod -x .github/scripts/install-git.sh
-
-chmod +x .github/scripts/configure-git.sh
-.github/scripts/configure-git.sh
-chmod -x .github/scripts/configure-git.sh
