54ccf29421c4d68bbe8e09052e0d3626286fca6f
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-17 01:00:17 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -1,7 +1,6 @@
 if [ ! -f "pyproject.toml" ]; then
   if [ -f "LICENSE" ]; then rm LICENSE ; fi
   if [ -f "README.md" ]; then rm README.md ; fi
-  if [ -f ".gitignore" ]; then rm .gitignore ; fi
 fi
 
 uv init $UV_PROJECT_TYPE .
@@ -9,6 +8,16 @@ uv init $UV_PROJECT_TYPE .
 uv tool install toml-cli && echo "TOML CLI added as UV tool."
 
 uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
+
+apk add wget
+
+if [ "$LICENSE_TYPE" = "MIT" ]; then
+  wget -O LICENSE https://raw.githubusercontent.com/IAmSorryDave/UvCorny/refs/heads/Setup/LICENSE
+else
+  wget -O LICENSE https://www.apache.org/licenses/LICENSE-2.0.txt
+fi
+
+apk del wget
 
 uv add --dev pytest && echo "Pytest Installed as Development Dependency"
 
