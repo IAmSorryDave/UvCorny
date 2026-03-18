00f90d4244df82ded375041f3e5e5c523e6970d6
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-18 13:32:13 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -2,20 +2,13 @@
 uv tool install toml-cli && echo "TOML CLI added as UV tool."
 
 if [ ! -f "pyproject.toml" ]; then
-  uv init $UV_PROJECT_TYPE . --force
+  if [ -f ".gitignore" ]; then rm .gitignore
+  if [ -f "README.md" ]; then rm README.md fi
+  if [ -f "LICENSE" ]; then rm LICENSE fi
+  uv init $UV_PROJECT_TYPE . --author-from git --vcs git
+  uv run toml set --toml-path pyproject.toml project.description "Hello world, this my cool app!"
   uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
 fi
-
-
-# apk add wget
-
-#if [ "$LICENSE_TYPE" = "MIT" ]; then
-#  wget -O LICENSE https://raw.githubusercontent.com/IAmSorryDave/UvCorny/refs/heads/Setup/LICENSE
-# else
-#  wget -O LICENSE https://www.apache.org/licenses/LICENSE-2.0.txt
-#fi
-
-# apk del wget
 
 if [ ! -f "requirements.txt" ]; then
   touch requirements.txt && uv add -r requirements.txt
