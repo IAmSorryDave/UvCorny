3aefc79b9c7b2dedc17956f2ef0ab9f1a3208592
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-18 12:50:55 -0700

    Update configure-project.sh
    Experiment

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -1,25 +1,25 @@
-if [ ! -f "pyproject.toml" ]; then
-  if [ -f "LICENSE" ]; then rm LICENSE ; fi
-  if [ -f "README.md" ]; then rm README.md ; fi
-fi
-
-uv init $UV_PROJECT_TYPE .
 
 uv tool install toml-cli && echo "TOML CLI added as UV tool."
 
-uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
-
-apk add wget
-
-if [ "$LICENSE_TYPE" = "MIT" ]; then
-  wget -O LICENSE https://raw.githubusercontent.com/IAmSorryDave/UvCorny/refs/heads/Setup/LICENSE
-else
-  wget -O LICENSE https://www.apache.org/licenses/LICENSE-2.0.txt
+if [ ! -f "pyproject.toml" ]; then
+  uv init $UV_PROJECT_TYPE . --description "Hello world" --force --vcs git
+  uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
 fi
 
-apk del wget
 
-touch requirements.txt
+# apk add wget
+
+#if [ "$LICENSE_TYPE" = "MIT" ]; then
+#  wget -O LICENSE https://raw.githubusercontent.com/IAmSorryDave/UvCorny/refs/heads/Setup/LICENSE
+# else
+#  wget -O LICENSE https://www.apache.org/licenses/LICENSE-2.0.txt
+#fi
+
+# apk del wget
+
+if [ ! -f "requirements.txt" ]; then
+  touch requirements.txt && uv add -r requirements.txt
+fi
 
 uv add --dev pytest && echo "Pytest Installed as Development Dependency"
 
