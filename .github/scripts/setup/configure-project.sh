6f6d785ee42b95e7ca45d34462d9227e02e7a86e
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-18 20:37:33 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -1,26 +1,28 @@
 
-uv tool install toml-cli && echo "TOML CLI added as UV tool."
-
 if [ ! -f "pyproject.toml" ]; then
-  if [ -f ".gitignore" ]; then rm .gitignore ; fi
+
+  uv tool install toml-cli && echo "TOML CLI added as UV tool."
+
   if [ -f "README.md" ]; then rm README.md ; fi
   if [ -f "LICENSE" ]; then rm LICENSE ; fi
-  uv init $UV_PROJECT_TYPE . --vcs git 
-  uv run toml set --toml-path pyproject.toml project.description "Hello world, this my cool app!"
-  uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
+  if [ -f ".gitignore" ]; then rm .gitignore ; fi
+  
+  uv init $UV_PROJECT_TYPE .
+
+  if [ ! -f "requirements.txt" ]; then touch requirements.txt ; fi
+
+  uv add -r requirements.txt
+
+  echo "# $(toml get --toml-path pyproject.toml project.name)" >> README.md && echo $PROJECT_DISCRIPTION >> README.md
+  
+  uv run toml set --toml-path pyproject.toml project.description $PROJECT_DISCRIPTION && uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
+  
+  uvx easyignore python
+
+  mkdir tests && touch tests/test_main.py && echo "import pytest" >> tests/test_main.py && echo "from $(toml get --toml-path pyproject.toml project.name) import main" >> tests/test_main.py
+  
 fi
 
-if [ ! -f "requirements.txt" ]; then
-  touch requirements.txt && uv add -r requirements.txt
-fi
-
-uv add --dev pytest && echo "Pytest Installed as Development Dependency"
-
-mkdir tests && touch tests/test_main.py
-echo "import pytest" >> tests/test_main.py
-echo "from $(toml get --toml-path pyproject.toml project.name) import main" >> tests/test_main.py
-
-uv add --dev ruff && echo "Ruff Installed as Development Dependency"
-uv run ruff format tests
+uv add --dev pytest && echo "Pytest Installed as Development Dependency" && uv add --dev ruff && echo "Ruff Installed as Development Dependency"
 
 echo "Project configured."
