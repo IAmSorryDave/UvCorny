3f27f3cdf7bfdd5d2c12d53421ed003ae6c89419
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-18 20:56:59 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -15,14 +15,15 @@ if [ ! -f "pyproject.toml" ]; then
 
   echo "# $(toml get --toml-path pyproject.toml project.name)" >> README.md && echo $PROJECT_DISCRIPTION >> README.md
   
-  uv run toml set --toml-path pyproject.toml project.description $PROJECT_DISCRIPTION && uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
+  uv run toml set --toml-path pyproject.toml project.description $PROJECT_DISCRIPTION 
+  uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
   
   uvx easyignore python
 
   mkdir tests && touch tests/test_main.py && echo "import pytest" >> tests/test_main.py && echo "from $(toml get --toml-path pyproject.toml project.name) import main" >> tests/test_main.py
+
+  uv add --dev pytest && echo "Pytest Installed as Development Dependency" && uv add --dev ruff && echo "Ruff Installed as Development Dependency"
+
+  echo "Project configured."
   
 fi
-
-uv add --dev pytest && echo "Pytest Installed as Development Dependency" && uv add --dev ruff && echo "Ruff Installed as Development Dependency"
-
-echo "Project configured."
