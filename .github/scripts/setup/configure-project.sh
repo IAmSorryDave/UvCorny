9ea1cac2fe0d2e006ced4752fc898174a1be4419
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-18 21:06:35 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -13,16 +13,18 @@ if [ ! -f "pyproject.toml" ]; then
 
   uv add -r requirements.txt
 
-  echo "# $(toml get --toml-path pyproject.toml project.name)" >> README.md && echo $PROJECT_DISCRIPTION >> README.md
-  
-  uv run toml set --toml-path pyproject.toml project.description $PROJECT_DISCRIPTION 
+  uv run toml set --toml-path pyproject.toml project.description $PYPROJECT_DISCRIPTION
   uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
+
+  echo "# $(toml get --toml-path pyproject.toml project.name) " >> README.md && echo $(toml get --toml-path pyproject.toml project.description) >> README.md
   
   uvx easyignore python
 
   mkdir tests && touch tests/test_main.py && echo "import pytest" >> tests/test_main.py && echo "from $(toml get --toml-path pyproject.toml project.name) import main" >> tests/test_main.py
 
   uv add --dev pytest && echo "Pytest Installed as Development Dependency" && uv add --dev ruff && echo "Ruff Installed as Development Dependency"
+
+  uv tool uninstall toml-cli && echo "TOML CLI removed as UV tool."
 
   echo "Project configured."
   
