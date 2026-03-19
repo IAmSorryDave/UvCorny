39777c0bb5e15586e9a8dc45b9cec79cdfbd9c5a
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-03-18 19:40:44 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -2,10 +2,10 @@
 uv tool install toml-cli && echo "TOML CLI added as UV tool."
 
 if [ ! -f "pyproject.toml" ]; then
-  if [ -f ".gitignore" ]; then rm .gitignore
+  if [ -f ".gitignore" ]; then rm .gitignore fi
   if [ -f "README.md" ]; then rm README.md fi
   if [ -f "LICENSE" ]; then rm LICENSE fi
-  uv init $UV_PROJECT_TYPE . --author-from git --vcs git
+  uv init $UV_PROJECT_TYPE .
   uv run toml set --toml-path pyproject.toml project.description "Hello world, this my cool app!"
   uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
 fi
@@ -19,9 +19,6 @@ uv add --dev pytest && echo "Pytest Installed as Development Dependency"
 mkdir tests && touch tests/test_main.py
 echo "import pytest" >> tests/test_main.py
 echo "from $(toml get --toml-path pyproject.toml project.name) import main" >> tests/test_main.py
-echo "def test_main():"  >> tests/test_main.py
-echo "  assert main() == None" >> tests/test_main.py
-echo "Testing directory configured."
 
 uv add --dev ruff && echo "Ruff Installed as Development Dependency"
 uv run ruff format tests
