c0a15d5a11930eb4697063600fc822ca9dd269a0
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-08 15:22:30 -0700

    Update configure-project.sh
    __init__ setup experiment

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -16,20 +16,48 @@ if [ ! -f "pyproject.toml" ]; then
   uv add -r requirements.txt
 
   echo "# $(toml get --toml-path pyproject.toml project.name ) " >> README.md && echo $(toml get --toml-path pyproject.toml project.description ) >> README.md
-  
-  uvx easyignore python
 
   mkdir tests && touch tests/test_main.py && echo "import pytest" >> tests/test_main.py && echo "from $(toml get --toml-path pyproject.toml project.name) import main" >> tests/test_main.py
 
-  uv add --dev pytest && echo "Pytest Installed as Development Dependency" && uv add --dev ruff && echo "Ruff Installed as Development Dependency" 
+  uv add --dev pytest && echo "Pytest Installed as Development Dependency" && uv add --dev ruff && echo "Ruff Installed as Development Dependency"
 
-  uv tool uninstall toml-cli && echo "TOML CLI removed as UV tool."
+  if [ $UV_PROJECT_TYPE == '--package' ]; then
+
+    echo "from importlib.metadata import version" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "__version__ = version('$(toml get --toml-path pyproject.toml project.name )')" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "del version" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "from importlib import import_module" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "from pathlib import Path" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "from pathlib import Path" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "for f in Path(__file__).parent.glob('*.py'):" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "  module_name = f.stem" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "  if (not module_name.startswith('_')) and (module_name not in globals()):" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "    import_module(f'.{module_name}', __package__)" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "  del f, module_name" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+
+    echo "del import_module, Path" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
+  
+  uvx easyignore python
+
+  uv tool uninstall toml-cli && echo "TOML CLI removed as UV tool." 
 
   echo "Project configured."
 
 else
 
   uv init
+
+  uv add -r requirements.txt
 
   uv add --dev pytest && echo "Pytest Installed as Development Dependency" && uv add --dev ruff && echo "Ruff Installed as Development Dependency"
 
