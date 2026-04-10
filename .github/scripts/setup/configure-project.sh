32bf87e9913605fd1985c768f0d79d92d2c571ca
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-10 10:40:22 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -23,7 +23,7 @@ if [ ! -f "pyproject.toml" ]; then
 
   if [ $UV_PROJECT_TYPE == '--package' ]; then
 
-    > "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py" # The default UV init file is pretty useless. Let's start fresh.
+    rm "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py" && cp .python/__init__.py src/$(toml get --toml-path pyproject.toml project.name )/  # The default UV init file is pretty useless. Tagging in custom __init__.py file.
 
     echo "from importlib.metadata import version" >> "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py"
 
