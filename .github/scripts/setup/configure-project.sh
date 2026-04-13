8f6969a0bcc44fa5f172ff27ec660b47eb1edc71
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-13 14:10:23 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -1,28 +1,27 @@
 
 # These files should be refreshed on every pull / push.
-if [ -f "README.md" ]; then rm README.md ; fi 
-if [ -f "requirements.txt"]; then rm requirements.txt ; fi
+if [ -f "README.md" ] then rm README.md  fi 
+if [ -f "requirements.txt"] then rm requirements.txt  fi
 
-uv add --dev -r requirements-dev.txt
-
-uv run pre-commit install
-
+# The absence of a TOML file indicates a project needs to be initialized.
 if [ ! -f "pyproject.toml" ]; then
 
   echo "Configuring project."
 
-  uv tool install toml-cli && echo "TOML CLI added as UV tool."
+  if [ -f "LICENSE" ] then rm LICENSE fi
+  if [ -f ".gitignore" ] then rm .gitignore fi
 
-  if [ -f "LICENSE" ]; then rm LICENSE ; fi
-  if [ -f ".gitignore" ]; then rm .gitignore ; fi
+  uv tool install toml-cli && echo "TOML CLI added as UV tool."
   
   uv init $UV_PROJECT_TYPE . && uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
+
+  uv add --dev -r requirements-dev.txt
 
   case "$UV_PROJECT_TYPE" in '--lib' | '--package')
 
     rm "src/$(toml get --toml-path pyproject.toml project.name )/__init__.py" && cp .python/templates/__init__.py src/$(toml get --toml-path pyproject.toml project.name )/  # The default UV init file is pretty useless. Tagging in custom __init__.py.
 
-    cp .python/templates/bootstrap.py src/$(toml get --toml-path pyproject.toml project.name )/ # And bootstrap.py script.
+    cp .python/templates/bootstrap.py src/$(toml get --toml-path pyproject.toml project.name )/ ;; # And bootstrap.py script.
      
   esac
     
@@ -39,3 +38,5 @@ else
   echo "Project configured." 
 
 fi
+
+uv run pre-commit install
