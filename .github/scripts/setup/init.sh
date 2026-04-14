e43c64122c90954bf4eb6b585a39983c92fdcedb
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-14 10:30:47 -0700

    Update init.sh

--- .github/scripts/setup/init.sh
+++ .github/scripts/setup/init.sh
@@ -4,3 +4,5 @@ chmod +x .github/scripts/setup/install-git.sh && .github/scripts/setup/install-g
 chmod +x .github/scripts/setup/configure-git.sh && .github/scripts/setup/configure-git.sh chmod -x .github/scripts/setup/configure-git.sh
 
 chmod +x .github/scripts/setup/configure-project.sh && .github/scripts/setup/configure-project.sh chmod -x .github/scripts/setup/configure-project.sh
+
+chmod +x .github/scripts/setup/install-bash.sh && .github/scripts/setup/install-bash.sh && chmod -x .github/scripts/setup/install-bash.sh
