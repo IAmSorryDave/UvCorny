93daf486f4d10b3e218b91078c8da3377fa425db
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-19 09:43:48 -0700

    Update init.sh

--- .github/scripts/setup/init.sh
+++ .github/scripts/setup/init.sh
@@ -7,3 +7,5 @@ chmod +x .github/scripts/setup/configure-git.sh && .github/scripts/setup/configu
 chmod +x .github/scripts/setup/install-bash.sh && .github/scripts/setup/install-bash.sh && chmod -x .github/scripts/setup/install-bash.sh
 
 chmod +x .github/scripts/setup/configure-project.sh && .github/scripts/setup/configure-project.sh chmod -x .github/scripts/setup/configure-project.sh
+
+chmod +x .github/scripts/setup/configure-pre-commit.sh && .github/scripts/setup/configure-pre-commit.sh && chmod -x .github/scripts/setup/configure-pre-commit.sh
