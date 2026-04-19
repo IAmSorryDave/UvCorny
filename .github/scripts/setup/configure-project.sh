6e814b45114c97b3f43491d8eaf161fbf5d04079
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-19 10:08:47 -0700

    Update configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -4,7 +4,7 @@
 if [ -f "README.md" ]; then rm README.md ; fi 
 
 # Generate blank requiements.txt if none found. Required for pre-commit install. Subsequent uv-export hooks will update this file.
-if [ ! -f "requirements.txt"]; then touch requirements.txt ; fi
+if [ ! -f "requirements.txt" ]; then touch requirements.txt ; fi
 
 # The absence of a TOML file indicates a project needs to be initialized.
 if [ ! -f "pyproject.toml" ]; then
