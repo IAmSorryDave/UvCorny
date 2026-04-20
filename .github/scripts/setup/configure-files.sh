a6cc382ac0a96e6b8657b922e3e0158ce986a038
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-20 14:12:59 -0700

    Update configure-files.sh

--- .github/scripts/setup/configure-files.sh
+++ .github/scripts/setup/configure-files.sh
@@ -6,8 +6,13 @@ if [ -f "README.md" ]; then rm README.md ; fi
 # Generate blank requiements.txt if none found. Required for pre-commit install. Subsequent uv-export hooks will update this file.
 if [ ! -f "requirements.txt" ]; then touch requirements.txt ; fi
 
+# Monkey patch .pre-commit-config.yaml
+
+## Have git ignore patch.
 git update-index --skip-worktree ${PWD}/.pre-commit-config.yaml
 
+## Add requirements flag
 yq eval '(.. | select(has("id") and .id == "pytest") | .additional_dependencies) += ["-r"]' -i ${PWD}/.pre-commit-config.yaml
 
+## Add absolute path of requirements file.
 yq eval '(.. | select(has("id") and .id == "pytest") | .additional_dependencies) += [strenv(PWD) + "/requirements.txt"]' -i ${PWD}/.pre-commit-config.yaml
