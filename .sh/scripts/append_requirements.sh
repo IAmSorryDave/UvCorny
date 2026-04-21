fc8bca769bd5a481afdbdc74f998ab1722800ffb
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-20 17:04:39 -0700

    Update append_requirements.sh

--- .sh/scripts/append_requirements.sh
+++ .sh/scripts/append_requirements.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-## Have git ignore patch.
-# git update-index --skip-worktree ${PWD}/.pre-commit-config.yaml
-
 ## Add requirements flag
 yq eval '(.. | select(has("id") and .id == "pytest") | .additional_dependencies) += ["-r"]' -i ${PWD}/.pre-commit-config.yaml
 
 ## Add absolute path of requirements file.
 yq eval '(.. | select(has("id") and .id == "pytest") | .additional_dependencies) += [strenv(PWD) + "/requirements.txt"]' -i ${PWD}/.pre-commit-config.yaml
 
+## Have git ignore patch.
 git update-index --assume-unchanged ${PWD}/.pre-commit-config.yaml
-
-# git update-index --no-kip-worktree ${PWD}/.pre-commit-config.yaml
