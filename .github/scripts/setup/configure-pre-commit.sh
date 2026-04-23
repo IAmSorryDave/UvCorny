8a44ba2c3b67ee5868eabede400f35f5cabf3e99
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-04-23 13:25:10 -0700

    Fix pytest hook removal in pre-commit config

--- .github/scripts/setup/configure-pre-commit.sh
+++ .github/scripts/setup/configure-pre-commit.sh
@@ -6,7 +6,7 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # Check if branch is alpha
 if [ "$BRANCH" = "main" ]; then
   # Delete the pytest hook from .pre-commit-config.yaml
-  yq 'del(.repos[] | select(.id == "pytest"))' -i .pre-commit-config.yaml
+  yq 'del(.repos[].hooks[] | select(.id == "pytest"))' -i .pre-commit-config.yaml
   echo "✓ Pytest hook removed from .pre-commit-config.yaml"
 fi
 
