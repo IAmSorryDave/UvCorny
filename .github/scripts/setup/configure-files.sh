7fd806578d585a47ad7db4afd67df0886935d5ac
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-05-01 20:45:05 -0700

    Remove README.md refresh on pull/push
    Removed outdated README.md refresh logic.

--- .github/scripts/setup/configure-files.sh
+++ .github/scripts/setup/configure-files.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# README.md should be refreshed on every pull / push.
-if [ -f "README.md" ]; then rm README.md ; fi 
-
 # Generate blank requiements.txt if none found. Required for pre-commit install. Subsequent uv-export hooks will update this file.
 if [ ! -f "requirements.txt" ]; then touch requirements.txt ; fi
 
