fa618497cae49d6022d07fdcff075b6a16452bd0
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-05-06 20:19:52 -0700

    Update configure-files.sh to check for existing tests
    Ensure test directory and conftest.py are created only if not present.

--- .github/scripts/setup/configure-files.sh
+++ .github/scripts/setup/configure-files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Setup test directory, init file, and custom conftest.py.
-mkdir tests && touch tests/__init__.py &&
+if [ ! -d "tests" ]; then mkdir tests && touch tests/__init__.py && cp .python/templates/conftest.py tests/ ; fi
 
 # Generate blank requiements.txt if none found. Required for pre-commit install. Subsequent uv-export hooks will update this file.
 if [ ! -f "requirements.txt" ]; then touch requirements.txt ; fi
