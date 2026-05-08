cfcac262bbd8a54a3438e9e473d16bb0b6fed56a
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-05-08 07:32:52 -0700

    Update version bump command in configure-project.sh

--- .github/scripts/setup/configure-project.sh
+++ .github/scripts/setup/configure-project.sh
@@ -12,7 +12,7 @@ if [ ! -f "pyproject.toml" ]; then
   
   uv init $UV_PROJECT_TYPE . && uv run toml set --toml-path pyproject.toml project.license $LICENSE_TYPE
 
-  uv version --bump major --bump dev
+  uv version "0.0.0"
 
   case "$UV_PROJECT_TYPE" in '--lib' | '--package')
 
