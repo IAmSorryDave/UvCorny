d72444b5ae77ffb2c1ce4456c892df525a391b82
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2026-05-09 17:02:05 -0700

    Refactor fixture functions for project directory path

--- .python/templates/conftest.py
+++ .python/templates/conftest.py
@@ -1,19 +1,17 @@
 import pytest
-import os  # Always import the os module at the top level. Doing otherwise will cause unexpected behavior.
-
 
 @pytest.fixture
-def curent_working_directory_path():
+def project_directory_path():
     from pathlib import Path
 
-    return Path(os.getcwd())
+    return Path(__file__).parent.parent
 
 
 @pytest.fixture
-def project_configuration(curent_working_directory_path):
+def project_configuration(project_directory_path):
     from tomllib import load
 
-    with open(curent_working_directory_path / "pyproject.toml", "rb") as f:
+    with open(project_directory_path / "pyproject.toml", "rb") as f:
         configuration = load(f)
     return configuration
 
